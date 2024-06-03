c0b742fe0965dbb805b07514650615d0dc0c96b9
Eugene Klimenko <eugklim1@gmail.com>
2024-06-03 20:20:15 +0300

    Updates

--- assets/instagram-ico-_jbTC7E3.js
+++ assets/instagram-ico-_jbTC7E3.js
@@ -1,4 +1,4 @@
-import{j as c}from"./index-fl5mZzTT.js";const e=({className:n})=>c.jsx("svg",{className:n,enableBackground:"new 0 0 512 512",version:"1.1",viewBox:"0 0 512 512",xmlSpace:"preserve",xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",children:c.jsx("g",{children:c.jsx("path",{d:`M308.3,508.5c-2.5,0.1-4.1,0.3-5.7,0.3c-34.2,0-68.3-0.1-102.5,0.1c-4.8,0-6.1-1.3-6.1-6.1
+import{j as c}from"./index-D1ApP2US.js";const e=({className:n})=>c.jsx("svg",{className:n,enableBackground:"new 0 0 512 512",version:"1.1",viewBox:"0 0 512 512",xmlSpace:"preserve",xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",children:c.jsx("g",{children:c.jsx("path",{d:`M308.3,508.5c-2.5,0.1-4.1,0.3-5.7,0.3c-34.2,0-68.3-0.1-102.5,0.1c-4.8,0-6.1-1.3-6.1-6.1
           c0.1-79.6,0.1-159.3,0.1-238.9c0-2.1,0-4.2,0-6.9c-18.6,0-36.7,0-55.1,0c0-28.4,0-56.3,0-85c1.9,0,3.7,0,5.4,0
           c15,0,30-0.1,45,0.1c3.8,0,4.8-1.1,4.8-4.8c-0.2-22.3-0.2-44.7,0-67c0.1-15.6,2.6-30.8,9.8-44.9c10.3-19.9,26.6-32.8,47.2-40.8
           c16.8-6.6,34.5-9,52.3-9.3c29-0.4,58-0.2,87-0.3c2.7,0,4.9-0.1,4.9,3.7c-0.1,27.5-0.1,55-0.1,82.5c0,0.3-0.1,0.6-0.5,1.9
